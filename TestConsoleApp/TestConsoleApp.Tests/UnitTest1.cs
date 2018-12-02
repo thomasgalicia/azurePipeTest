82cf9fc6d062cd7649f3bff3d4a3eb2472c2a58b
Thomas Galicia <11601037@student.pxl.be>
2018-12-02 13:57:56 +0100

    Add first test

---
+++ TestConsoleApp/TestConsoleApp.Tests/UnitTest1.cs
@@ -0,0 +1,14 @@
+using NUnit.Framework;
+
+namespace TestConsoleApp.Tests
+{
+    [TestFixture]
+    public class UnitTest1
+    {
+        [Test]
+        public void TestMethod1()
+        {
+            Assert.True(true);
+        }
+    }
+}
